c1deb58d6c6cb03e6873d0c449e9e058bd736b60
rafiulhasanarb <rafiulhasanarb@gmail.com>
2025-04-24 16:05:05 +0600

    Initial Commit

---
+++ WaterTracker/WaterTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterTrackerApp.swift
+//  WaterTracker
+//
+//  Created by Rafiul Hasan on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct WaterTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
