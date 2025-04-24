64f458ea4a138fe278d5de99079a92dd7fcf97fe
rafiulhasanarb <rafiulhasanarb@gmail.com>
2025-04-24 22:40:22 +0600

    tabbarview added

---
+++ WaterTracker/Views/TabBarView.swift
@@ -0,0 +1,56 @@
+//
+//  TabBarView.swift
+//  WaterTracker
+//
+//  Created by Rafiul Hasan on 4/24/25.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    
+    @State private var selectedTabIndex = 0
+    
+    var body: some View {
+        TabView(selection: $selectedTabIndex) {
+            Text("Home")
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+                .tag(0)
+            
+            Text("Analysis")
+                .tabItem {
+                    Image(systemName: "chart.bar.xaxis")
+                    Text("Analysis")
+                }
+                .tag(1)
+            
+            Text("Alerm")
+                .tabItem {
+                    Image(systemName: "stopwatch")
+                }
+                .tag(2)
+            
+            Text("Settings")
+                .tabItem {
+                    Image(systemName: "gear")
+                    Text("Settings")
+                }
+                .tag(3)
+            
+            Text("Profile")
+                .tabItem {
+                    Image(systemName: "person")
+                    Text("Profile")
+                }
+                .tag(4)
+        }
+        .tint(.themePrimary)
+    }
+}
+
+#Preview {
+    TabBarView()
+}
